affd9f9ef6a43a5f4ae43acd8000b97277b3d45c
andrey-kupets <andrey.kupets@i.ua>
2021-05-16 00:35:29 +0300

    ...

--- react_trainings/src/App.js
+++ react_trainings/src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-const Header = ({add}) => (
-    <h1>{add}</h1>
-)
+const Header = ({counter}) => {
+    console.log('rerender child');
+    return <h1>{counter}</h1>;
+}
 
 function App() {
-  const [counter, setCounter ] = useState(0);
-  const [headerVisibility, setHeaderVisibility ] = useState(true);
-  const [todos, setTodos] = useState(['react', 'angular']);
+  console.log('rerender parent');
+
+  const [ counter, setCounter ] = useState(0);
+  const [ headerVisibility, setHeaderVisibility ] = useState(true);
+  const [ todos, setTodos ] = useState([ 'react', 'angular' ]);
 
   const countHandler = () => {
     setCounter(counter + 1);
@@ -24,14 +27,19 @@ function App() {
     const newArr = [...todos];
     newArr[1] = Math.random();
     setTodos(newArr);
-    // if ref on initialState isn't changed - NO RENDER - so we copy by ... or in that way:
-    //   todos = [todos[0], todos[1]];
+
+    // if ref on initialState isn't changed - NO RENDER - so we copy by ...
+      // or in that way:
+      // setTodos([...todos])
+      // todos = [todos[0], todos[1]];
     //   todos[1] = Math.random();
+    //   setTodos(todos);
+
   };
 
   return (
     <div className="App">
-      {headerVisibility && <Header add={counter}/>}
+      {headerVisibility && <Header counter={counter}/>}
       <button onClick={countHandler}><h3>+1</h3></button>
       <button onClick={toggleHandler}><h3>toggleHeader</h3></button>
       <button onClick={changeTodo}><h3>change todo</h3></button>
