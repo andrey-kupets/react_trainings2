8c24684cb473990314ad05722ce5462af422b704
andrey-kupets <andrey.kupets@i.ua>
2021-06-04 10:23:51 +0300

    fetch by router

--- react_trainings2/src/App.js
+++ react_trainings2/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,14 +16,17 @@ export default function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
+            {/*<li>*/}
+            {/*  <Link to="/about">About</Link>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+            {/*  <Link to="/users">Users</Link>*/}
+            {/*</li>*/}
+            {/*<li>*/}
+            {/*  <Link to="/test">Test</Link>*/}
+            {/*</li>*/}
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/test">Test</Link>
+              <Link to="/posts">Posts</Link>
             </li>
           </ul>
         </nav>
@@ -31,32 +34,35 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          {/*different ways to render*/}
           {/*<Route path="/" exact="true">*/}
           {/*  <Home />*/}
           {/*</Route>*/}
-          <Route path="/" component={Home} exact/>
-
-          <Route path="/about" render={(args) => {
-            console.log(args);
-            return <About/>
-          }}/>
-
+          {/*<Route path="/about" render={(args) => {*/}
+          {/*  console.log(args);*/}
+          {/*  return <About/>*/}
+          {/*}}/>*/}
           {/*doesn't conduct props*/}
-          <Route path="/users">
-            <Users />
-          </Route>
-
-          <Route path="/test">
-            {Test}
-          </Route>
-
+          {/*<Route path="/users">*/}
+          {/*  <Users />*/}
+          {/*</Route>*/}
+          {/*<Route path="/test">*/}
+          {/*  {Test}*/}
+          {/*</Route>*/}
           {/*<Route>*/}
-          {/*  <h2>PAGE'S NOT FOUND</h2>*/}
+          {/*  <Redirect to="/"/>*/}
           {/*</Route>*/}
 
-          <Route>
-            <Redirect to="/"/>
+          <Route path="/" component={Home} exact/>
+
+          <Route path="/posts">
+            <Posts/>
           </Route>
+
+          <Route>
+            <h2>PAGE'S NOT FOUND</h2>
+          </Route>
+
         </Switch>
       </div>
     </Router>
@@ -68,17 +74,30 @@ function Home(props) {
   return <h2>Home</h2>;
 }
 
-function About(props) {
-  // console.log(props)
-  return <h2>About</h2>;
+function Posts(props) {
+  console.log(props)
+  const [posts, setPosts] = useState([]);
+
+  const fetchData = async () => {
+    const baseUrl = 'https://jsonplaceholder.typicode.com/posts/';
+
+    const rawData = await fetch(baseUrl);
+    const jsonData = await rawData.json();
+
+    setPosts(jsonData);
+    console.log(posts);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((el) => <li>{el.id} - {el.title}</li>)}
+      </ul>
+    </div>
+  );
 }
 
-function Users(props) {
-  console.log(props)
-  return <h2>Users</h2>;
-}
-
-function Test(props) {
-  console.log(props)
-  return <h2>Test</h2>;
-}
