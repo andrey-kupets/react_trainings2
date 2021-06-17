558329746f4f4f22b3c70ec3408e67a6efb64802
andrey-kupets <andrey.kupets@i.ua>
2021-06-17 23:21:13 +0300

    3. Redux: product details rendering

--- react_trainings2/src/components/ProductDetails.js
+++ react_trainings2/src/components/ProductDetails.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import {Product} from "./Product";
 
 export const ProductDetails = () => {
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState(null);
   const params = useParams();
-  console.log(params)
+
+  const fetchItem = async () => {
+    if (!params.id) return;
+
+    try {
+      setLoading(true);
+      const rawData = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+      const jsonData = await rawData.json();
+      setProduct(jsonData);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchItem();
+  }, [])
 
   return (
-    <div className='product-item'>
-      {/*<Product/>*/}777
+    <div>
+      {/*в компоненту Продакт передаю пропсу "продакт" дважды - с этой компоненты и с Продактс*/}
+      {/*и смотря с откуда производится рендер, т.е. как мы реЮзаем Продакт такое значение пропса и принимает*/}
+      {!loading && !!product && <Product product={product}/>}
+      {loading && (
+        <h3>loading...</h3>
+      )}
     </div>
   )
 }
